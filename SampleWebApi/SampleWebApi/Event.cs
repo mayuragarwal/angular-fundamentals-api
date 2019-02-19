f18660d80d6f0137f4d0616836f55f8220d91bfe
Mayur Agarwal <mayuragarwal7@outlook.com>
2019-02-19 23:51:01 +0530

    initisl commit

---
+++ SampleWebApi/SampleWebApi/Event.cs
@@ -0,0 +1,33 @@
+﻿namespace SampleWebApi
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public double Price { get; set; }
+        public string ImageUrl { get; set; }
+        public string OnlineUrl { get; set; }
+        public Location Location { get; set; }
+        public Session[] Sessions { get; set; }
+    }
+
+    public class Session
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Presenter { get; set; }
+        public int Duration { get; set; }
+        public string Level { get; set; }
+        public string Abstract { get; set; }
+        public string[] Voters { get; set; }
+    }
+
+    public class Location
+    {
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
